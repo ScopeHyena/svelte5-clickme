ac93702bbbc43c3dca9c314d6d36280405b3ef15
Managed via Tart <admin@Manageds-Virtual-Machine.local>
2026-04-21 05:12:27 +0000

    Init: Svelte 5 Project Initialization

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    let messages = $state("");
+
+    function showMessage() {
+        messages = "Hello, World!";
+    }
+</script>
+
+<main>
+    <input type="text" bind:value={ messages } placeholder="push the button." />
+    <button onclick = { showMessage }>
+        Click Me
+    </button>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        max-width: 200px;
+        margin: 2rem;
+    }
+
+    input, button {
+        padding: 0.5rem;
+        font-size: 1rem;
+    }
+</style>
