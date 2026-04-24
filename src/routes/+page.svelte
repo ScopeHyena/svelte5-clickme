fa1941eb1ec651e27ea206592da9584e6d913a0a
Scope.H <hongsch07@naver.com>
2026-04-24 08:55:51 +0000

    Added New UI Style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import UILayout from "./UILayout.svelte";
     let messages = $state("");
 
     function showMessage() {
@@ -6,27 +7,38 @@
     }
 </script>
 
-<main>
-    <input type="text" bind:value={ messages } placeholder="push the button." />
-    <button onclick = { showMessage }>
+<UILayout title="Hello, World">
+    <div class="card">
+        <input class="card" type="text" bind:value={ messages } placeholder="Push the Button." />
+    </div>
+    
+    <button class="card" onclick={ showMessage }>
         Click Me
     </button>
-</main>
+
+</UILayout>
 
 <style>
-    main {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        max-width: 200px;
-        margin: 2rem;
+    .card {
+        background-color: white;
+        border-radius: 18px;
+        padding: 20px 24px;
+        margin-bottom: 16px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.03);
+        font-size: 1.1rem;
+        font-weight: 500;
+        color: #1c1c1e;
     }
 
-    input, button {
-        padding: 0.5rem;
-        font-size: 1rem;
+    input {
+        max-width: 100%;
+        border: 1px solid rgba(0, 0, 0, 0.05);
     }
+
+    button {
+        width: 100%;
+        text-align: start;
+        color: #ff3e00;
+    }
+
 </style>
-
-
-
